f1bc55296170d62ed087c8d6fc54329b44fa2226
Vikas Kumar Vishwakarma <vikas@vikass-MacBook-Air.local>
2021-12-28 14:46:36 +0530

    cros

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const request = require("request")
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors({
+    origin: '*'
+}));
+
 app.get('/', (req, res) => {
     res.send("Welcome to Covid App")
 })
